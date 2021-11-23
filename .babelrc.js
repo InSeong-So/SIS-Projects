9c3b8eb871b50775cee9073a5a65022c0ce6a3dd
InSeong-So <goflvhxj2547@gmail.com>
2021-11-23 11:11:52 +0900

    🔬Chore: 바벨, TS 설정

--- .babelrc.js
+++ .babelrc.js
@@ -4,9 +4,20 @@ module.exports = {
       '@babel/preset-env',
       {
         targets: {
+          edge: "17",
+          firefox: "60",
+          chrome: "67",
+          safari: "11.1",
+          ie: "11",
           node: 'current',
         },
       },
     ],
   ],
+  "plugins": [[
+    '@babel/plugin-transform-runtime',
+    {
+      regenerator: true,
+    },
+  ]],
 };
